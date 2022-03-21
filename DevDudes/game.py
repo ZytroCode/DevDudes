aecd1dcad7a680f650af4681b3a20f055e6df036
Zytro <omarwt92@gmail.com>
2022-03-21 16:01:04 +0800

    Basic setup for the game

--- DevDudes/game.py
+++ DevDudes/game.py
@@ -1,9 +1,39 @@
 import pygame
+
+# Initializing pygame
 pygame.init()
 
+# Game Class
 class Game:
-	def __init__(self):
-		self.running = False
+    def __init__(self, width, height, title):
+        # Setups
+        self.SIZE = self.WIDTH, self.HEIGHT = width, height
+        self.title = title
+        self.clock = pygame.time.Clock()
+        self.fps = 100
+        self.running = False
 
-	def run(self):
-		self.running = True
+        # Window creation
+        self.window = pygame.display.set_mode(self.SIZE, flags=pygame.HIDDEN)
+        pygame.display.set_caption(self.title)
+
+    def run(self):
+        # Showing the window
+        self.window = pygame.display.set_mode(self.SIZE, flags=pygame.SHOWN)
+        self.running = True
+
+        # Game Loop
+        while self.running:
+
+            # Listening for pygame events
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # Updating the display and ticking the clock
+            pygame.display.update()
+            self.clock.tick(self.fps)
+
+        # Uninitializing pygame
+        pygame.quit()
--- DevDudes/main.py
+++ DevDudes/main.py
@@ -1,7 +1,7 @@
 from game import Game
 
 def main():
-    game = Game()
+    game = Game(800, 600, "DevDudes - PyWeek 33")
     game.run()
 
 if __name__ == "__main__":
